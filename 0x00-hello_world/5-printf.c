ffae4262781e7e23b57fb12117f64dfaf0af2df8
nada-cmd <n192004a@gmail.com>
2023-03-16 12:59:11 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
+#include <stio.h>
 /**
- * main - A Program that print a line using the printf function
- * Return: 0(Seccess)
+ * main - prints "with proper grammar, but the outcome is a piece of art," followed newline
+ * Return: Always 0.
+ *
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n")
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
 }
