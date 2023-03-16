1691e966d34271bf8bb6659421f22ec10d50c9d8
nada-cmd <n192004a@gmail.com>
2023-03-16 13:37:27 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,5 @@ int main(void)
 	printf("Size of a int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	return (0)
+	return (0);
 }
