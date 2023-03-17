46095bb13c38ebe97d115b2b9c605f05efe64479
nada-cmd <n192004a@gmail.com>
2023-03-17 13:15:54 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
+
 #include <time.h>
+
 #include <stdio.h>
 /**
  * main - prints a text according number
@@ -11,6 +13,7 @@ int main(void)
 int n, lastd;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+lastd = n % 10;
 if (lastd > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
