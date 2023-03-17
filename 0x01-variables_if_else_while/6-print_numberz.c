0916d9d73b3d9e961686b342488b5627382402be
nada-cmd <n192004a@gmail.com>
2023-03-17 14:14:58 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always (Success)
+ *
+ */
+int main(void)
+{
+int d;
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+putchar('\n');
+return (0);
+}
