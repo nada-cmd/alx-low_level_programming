20f521d62eebe91fdc5ccf9fd092674ac9eaad95
nada-cmd <n192004a@gmail.com>
2023-03-27 11:15:11 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,28 +1,20 @@
 #include "main.h"
 /**
- * puts2 - function should print only one character out a
- * starting with the first one
- * @str: input
- * Return: print
+ * puts2 - function should print only one character of a string
+ * @str: The string to be treated
+ * Return: void
  */
 void puts2(char *str)
 {
-int longi = 0;
-int t = 0;
-char *y = str;
-int o;
-while (*y != '\0')
+int i;
+int j = 0;
+while (str[j] != '\0')
 {
-y++;
-longi++;
+j++;
 }
-t = longi - 1;
-for (o = 0; o < = t; o++)
+for (i = 0; i < j; i += 2)
 {
-if (o % 2 == 0)
-{
-_putchar(str[o]);
-}
+_putchar(str[i]);
 }
 _putchar('\n');
 }
