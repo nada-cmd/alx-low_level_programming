ff3991813785150a8d7b005ae419715795744685
nada-cmd <n192004a@gmail.com>
2023-03-27 10:42:20 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,20 +3,17 @@
  * rev_string - Reverse a string
  * @s: Input string
  * Return: string in reverse
- * Return: Always 0 (success)
  */
 void rev_string(char *s)
 {
-char rev = s[0];
-int counter = 0;
-int i;
-while (s[counter] != '\n')
-counter++;
-for (i = 0; i < counter; i++)
+int len = 0, index = 0;
+char tmp;
+while (s[index])
+len++;
+for (index = len - 1; index >= len / 2; index--)
 {
-counter--;
-rev = s[i];
-s[i] = s[counter];
-s[counter] = rev;
+tem = s[index];
+s[index] = s[len - index - 1];
+s[len - index - 1] = tmp;
 }
 }
