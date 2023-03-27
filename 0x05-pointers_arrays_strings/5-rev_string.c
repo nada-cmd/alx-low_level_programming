b0b376111170b11c1066dfb0ee4e890b1d96dece
nada-cmd <n192004a@gmail.com>
2023-03-27 09:01:06 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,10 +10,10 @@ int len = 0, index = 0;
 char tmp;
 while (s[index++])
 len++;
-for (index = len -1; index >= len /2; index--)
+for (index = len - 1; index >= len / 2; index--)
 {
 tmp = s[index];
-s[index] = s[len - index -1];
-s[len - inddex -1] = tmp;
+s[index] = s[len - index - 1];
+s[len - inddex - 1] = tmp;
 }
 }
