511cde1011e0792039dfc00ed37d3f305ee0efe4
nada-cmd <n192004a@gmail.com>
2023-03-29 14:03:52 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,19 +4,24 @@
  * _strcat - Concaenation two strings
  * @dest: The destination string
  * @src: The source string
- * Return: A pointer to the resulting string dest
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
-int dlen = 0, i;
-while (dest[dlen])
+int i;
+int j;
+i = 0;
+while (dest[i] != '\0')
 {
-dlen++;
+i++;
 }
-for (i = 0; src[i] != 0; i++)
+j = 0;
+while (src[j] != '\0')
 {
-dest[dlen] = src[i];
+dest[i] = src[j];
+i++;
+j++;
 }
-dest[dlen] = '\0';
+dest[i] = '\0';
 return (dest);
 }
