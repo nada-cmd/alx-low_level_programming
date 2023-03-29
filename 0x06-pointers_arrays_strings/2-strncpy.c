f0859932f335a974423675d2df369d75489af24d
nada-cmd <n192004a@gmail.com>
2023-03-29 14:37:30 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,17 +8,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int j;
-j = 0;
-while (j < n && src[j] != '\0')
-{
-dest[j] = src[j];
-j++;
-}
-while (j < n)
-{
-dest[j] = '\0';
-j++
-}
+int index = 0;
+src_len = 0;
+while (src[index++])
+src_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[index] = src[index];
+for (index = src_len; index < n; index++)
+dest[index] = '\0';
 return (dest);
 }
