ec03918814611360348c8726c78d5ff81b756488
nada-cmd <n192004a@gmail.com>
2023-03-29 13:54:19 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-main.c
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -10,7 +10,7 @@ char s1[98] = "Hello ";
 char s2[] = "World!\n";
 char *ptr;
 printf("%s\n", s1);
-printf("%s\n", s2);
+printf("%s", s2);
 ptr = _strcat(s1, s2);
 printf("%s", s1);
 printf("%s", s2);
