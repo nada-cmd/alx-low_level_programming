03934d04ac587402c87efd6429802b04a7a4d146
nada-cmd <n192004a@gmail.com>
2023-03-30 07:00:14 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * cap_string - capitalizes all words of a string.
- * @str: The string to be capitalized.
+ * Cap_string - Capitalizes all words of a string.
+ * @str: The string to be Capitalized.
  * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
@@ -9,7 +9,7 @@ char *cap_string(char *str)
 int index = 0;
 while (str[index])
 {
-while (!(str[index] >= 'a' && str[inddex] <= 'z'))
+while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
 if (str[index - 1] == ' ' ||
 str[index - 1] == '\t' ||
