00436e2567840d673e2cfdef32fc95a3eaa3577d
nada-cmd <n192004a@gmail.com>
2023-03-30 06:31:46 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * string_touuper - change all lowercase to uppercase
+ * string_toupper - change all lowercase to uppercase
  * @n: pointer
  * Return: n
  */
-char *string_touuper(char *n)
+char *string_toupper(char *n)
 {
 int i;
 i = 0;
