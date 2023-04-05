80a54f65cc62382bae53a80d23d89da68d86b986
nada-cmd <n192004a@gmail.com>
2023-04-05 06:11:02 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,7 +3,7 @@ int check_pal(char *s, int i, int len);
 int _strlen_recursion(char *s);
 /**
  * is_palindrome - checks if a string is a palindrome
- * @s: string to reversw
+ * @s: string to reverse
  * Return: 1 if it is, 0 it's not
  */
 int is_palindrome(char *s)
