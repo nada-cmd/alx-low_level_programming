0128261d8d876994923f9b68e3ec1949f74428a7
nada-cmd <n192004a@gmail.com>
2023-04-13 12:17:03 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
- * *malloc_checked - allocates memory using malloc
- * @b: the number of bytes to allocate
- * Return: a pointer to the allocated memory
+ * *malloc_checked - Alocates memory
+ * @b: the size to allocate
+ * Return: Nothing.
  */
 void *malloc_checked(unsigned int b)
 {
-void *ptr;
-ptr = malloc(b);
-if (ptr == NULL)
+void *p;
+p = malloc(b);
+if (p == NULL)
 exit(98);
-return (ptr);
+return (p);
 }
