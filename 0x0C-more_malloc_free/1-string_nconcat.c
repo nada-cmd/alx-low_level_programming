67d1b735064ebd58f3eadca05be1d161c8f41902
nada-cmd <n192004a@gmail.com>
2023-04-13 12:43:54 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * string_noconcat - ...
- * @s1: ...
- * @s2: ...
- * @n: ...
- * Return: ...
+ * string_noconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
+ * Return: pointer to the resulting string
  */
 char *string_noconcat(char *s1, char *s2, unsigned int n)
 {
