b893980144d1b27d58ae01087c196edba294a2de
nada-cmd <n192004a@gmail.com>
2023-04-17 11:56:56 -0700

    2-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print the name of the file it was complied from
+ * Return: nothing to be returned
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
