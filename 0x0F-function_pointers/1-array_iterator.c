f06d410c3c5212ac79610a63e671b9ed5c27ff13
nada-cmd <n192004a@gmail.com>
2023-04-19 10:36:18 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - prints each array elam on a newl
+ * @array: array
+ * @size: how many elam to print
+ * @action: pointer to print in regular or hexx
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i;
+if (array == NULL || action == NULL)
+return;
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
